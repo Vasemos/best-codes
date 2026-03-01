3ae1381ff460610bb7993ecd1ca3f376e52572b0
Vladimir <poshtaowla@gmail.com>
2026-03-01 19:45:20 +0500

    Add Regex codes and receipt passer

---
+++ Practice5/Regex/match_object.py
@@ -0,0 +1,17 @@
+import re
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
+
+txt = "The rain in Spain"
+x = re.search(r"\bS\w+", txt)
+print(x.group())
